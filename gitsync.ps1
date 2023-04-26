89fcf002c4be09c71553a44bcfb6a4f756fb4aee
n4krug <gustav.eneberg@gmail.com>
2023-04-27 01:04:43 +0200

    K-2SO Sync Thu Apr 27 01:04:43 2023 +02:00

--- gitsync.ps1
+++ gitsync.ps1
@@ -1,4 +1,5 @@
 Set-Location "C:\Users\gusta\Documents\Obsidian Main Vault\Main Vault\"
+Get-Content token.txt | gh auth login --with-token
 git add .
 git commit -m "K-2SO Sync $(Get-Date -format 'ddd MMM d HH:mm:ss yyyy zzz')"
 git pull
